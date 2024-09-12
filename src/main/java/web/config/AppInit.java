b6b5983fbaef25f52e008db26b7a154fa337c6e3
onlaynik <50167275+onlaynik@users.noreply.github.com>
2024-09-12 20:36:12 +0600

    Update AppInit.java

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -34,6 +34,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     }
 
 
+
     private void registerHiddenFieldFilter(ServletContext aContext) {
         aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
